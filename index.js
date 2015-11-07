2acee7a0719559bbbdc8f0d171556ad8fbdd74b8
benji6 <benhall22@hotmail.co.uk>
2015-11-07 17:27:45 +0000

    reorder

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 const callWithIterator = f => iterable => f(iterable[Symbol.iterator]());
 const createIterable = generator => ({[Symbol.iterator]: generator});
 
-export const range = a => b => createIterable(function* (n = a) {
-  if (n < b) while (n <= b) yield n++;
-  else while (n >= b) yield n--;
-});
-
-export const reduce = f => acc => callWithIterator(iterator => {
-  while (true) {
-    let {value, done} = iterator.next();
-    if (done) return acc;
-    acc = f(acc)(value);
-  }
-})
-
 export const filter = f => iterable => createIterable(function* () {
   for (let val of iterable) if (f(val)) yield val;
 });
@@ -36,19 +23,32 @@ export const findIndex = f => callWithIterator(iterator => {
   }
 });
 
+export const makeCircular = iterable => createIterable(function* () {
+  while (true) yield* iterable[Symbol.iterator]();
+});
+
 export const map = f => iterable => createIterable(function* () {
   for (let val of iterable[Symbol.iterator]()) yield f(val);
 });
-
-export const take = a => callWithIterator(iterator => createIterable(function* (n = a) {
-  while (n--) yield iterator.next().value;
-}));
 
 export const nth = n => callWithIterator(iterator => {
   while (n--) iterator.next();
   return iterator.next().value;
 });
 
-export const makeCircular = iterable => createIterable(function* () {
-  while (true) yield* iterable[Symbol.iterator]();
+export const range = a => b => createIterable(function* (n = a) {
+  if (n < b) while (n <= b) yield n++;
+  else while (n >= b) yield n--;
 });
+
+export const reduce = f => acc => callWithIterator(iterator => {
+  while (true) {
+    let {value, done} = iterator.next();
+    if (done) return acc;
+    acc = f(acc)(value);
+  }
+});
+
+export const take = a => callWithIterator(iterator => createIterable(function* (n = a) {
+  while (n--) yield iterator.next().value;
+}));
--- test.js
+++ test.js
@@ -10,53 +10,9 @@ const add = a => b => a + b;
 const double = x => x * 2;
 const halve = x => x / 2;
 
-test('range', t => {
-    t.deepEquals([...range(1)(1)],
-                 [1]);
-    t.deepEquals([...range(1)(3)],
+test('filter', t => {
+    t.deepEquals([...filter(x => x <= 3)(range(1)(100))],
                  oneTwoThree);
-    t.deepEquals([...range(3)(1)],
-                 threeTwoOne);
-    t.end();
-});
-
-test('reduce', t => {
-    t.deepEquals(reduce(add)(0)(oneTwoThree),
-                 6);
-    t.end();
-});
-
-test('map', t => {
-    t.deepEquals([...map(halve)([2, 4, 6])],
-                 oneTwoThree);
-    t.end();
-});
-
-test('take', t => {
-    t.deepEquals([...take(3)([1, 2, 3, 4])],
-                 oneTwoThree);
-    t.deepEquals([...take(3)(positiveIntegers)],
-                 oneTwoThree);
-    t.end();
-});
-
-test('take map', t => {
-    t.deepEquals([...map(double)(take(3)(positiveIntegers))],
-                 [2, 4, 6]);
-    t.end();
-});
-
-test('map take', t => {
-    t.deepEquals([...take(3)(map(double)(positiveIntegers))],
-                 [2, 4, 6]);
-    t.end();
-});
-
-test('nth', t => {
-    t.deepEquals(nth(0)(positiveIntegers),
-                 1);
-    t.deepEquals(nth(1)(positiveIntegers),
-                 2);
     t.end();
 });
 
@@ -80,14 +36,58 @@ test('findIndex', t => {
     t.end();
 });
 
-test('filter', t => {
-    t.deepEquals([...filter(x => x <= 3)(range(1)(100))],
+test('map', t => {
+    t.deepEquals([...map(halve)([2, 4, 6])],
                  oneTwoThree);
+    t.end();
+});
+
+test('nth', t => {
+    t.deepEquals(nth(0)(positiveIntegers),
+                 1);
+    t.deepEquals(nth(1)(positiveIntegers),
+                 2);
+    t.end();
+});
+
+test('range', t => {
+    t.deepEquals([...range(1)(1)],
+                 [1]);
+    t.deepEquals([...range(1)(3)],
+                 oneTwoThree);
+    t.deepEquals([...range(3)(1)],
+                 threeTwoOne);
+    t.end();
+});
+
+test('reduce', t => {
+    t.deepEquals(reduce(add)(0)(oneTwoThree),
+                 6);
+    t.end();
+});
+
+test('take', t => {
+    t.deepEquals([...take(3)([1, 2, 3, 4])],
+                 oneTwoThree);
+    t.deepEquals([...take(3)(positiveIntegers)],
+                 oneTwoThree);
+    t.end();
+});
+
+test('map take', t => {
+    t.deepEquals([...take(3)(map(double)(positiveIntegers))],
+                 [2, 4, 6]);
     t.end();
 });
 
 test('take makeCircular', t => {
     t.deepEquals([...take(8)(makeCircular(range(1)(3)))],
                  [1, 2, 3, 1, 2, 3, 1, 2]);
+    t.end();
+});
+
+test('take map', t => {
+    t.deepEquals([...map(double)(take(3)(positiveIntegers))],
+                 [2, 4, 6]);
     t.end();
 });
